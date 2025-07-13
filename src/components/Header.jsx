36f9241cd82b74eee423125b6b47341662e02520
Abolfazl <boorbooryabolfazl85@gmail.com>
2025-07-13 21:49:01 +0330

    change path to auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
         </div>
         <div className="w-1/2 flex justify-end">
           <li>
-            <NavLink path="/signin" active="bg-primary-400">
+            <NavLink path="/auth" active="bg-primary-400">
               ورود | ثبت نام
             </NavLink>
           </li>
