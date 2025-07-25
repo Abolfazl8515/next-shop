2b2fd50f0a153ebb9f33acfda0e48078333af54e
Abolfazl <boorbooryabolfazl85@gmail.com>
2025-07-25 14:46:46 +0330

    user dashboard sample

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,15 @@
 function UserProfilePage() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-secondary-700">داشبورد</h1>
+      <div className="mt-4">
+        <h3 className="text-xl font-bold text-secondary-700">
+          آخرین سفارش های شما
+        </h3>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default UserProfilePage;
