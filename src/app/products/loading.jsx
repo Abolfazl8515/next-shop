1b5f5b7a8ad1fd845772c121a61c63fbf95d14b8
Abolfazl <boorbooryabolfazl85@gmail.com>
2025-08-11 14:49:56 +0330

    products page loading

---
+++ src/app/products/loading.jsx
@@ -0,0 +1,10 @@
+import Spinner from "@/ui/Loading";
+
+function Loading() {
+  return (
+    <div className="grid items-center justify-center gap-x-4">
+      <Spinner />
+    </div>
+  );
+}
+export default Loading;
