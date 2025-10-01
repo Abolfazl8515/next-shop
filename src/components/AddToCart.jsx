60ae026f9f6eb9125017320dad9df520f223e4ec
Abolfazl <boorbooryabolfazl85@gmail.com>
2025-10-01 12:50:53 +0330

    increase and decrease in add to cart

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useAuth } from "@/context/AuthProvider";
-import { addToCartApi } from "@/services/cartService";
+import { addToCartApi, deleteFromCartApi } from "@/services/cartService";
 import Button from "@/ui/Button";
 import SpinnerMini from "@/ui/SpinnerMini";
 import { useRouter } from "next/navigation";
@@ -8,9 +8,12 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 
 function AddToCart({ id, className }) {
-  const { user, getUser } = useAuth();
+  const { user, getUser, cart } = useAuth();
   const { push } = useRouter();
   const [loading, setLoading] = useState(false);
+  const selectedProduct = cart?.productDetail?.find(
+    (product) => String(product._id) === String(id)
+  );
 
   const addToCartHandler = async () => {
     if (!user) {
@@ -31,7 +34,35 @@ function AddToCart({ id, className }) {
     }
   };
 
-  return (
+  const decreaseHandler = async () => {
+    try {
+      const { message } = await deleteFromCartApi({ productId: id });
+      toast.success(message);
+      getUser();
+    } catch (err) {
+      toast.error(err?.response?.data?.message);
+    }
+  };
+
+  return selectedProduct?.quantity > 0 ? (
+    <div className="flex items-center gap-2">
+      <Button
+        variant="danger"
+        className="h-8 flex justify-center items-center"
+        onClick={decreaseHandler}
+      >
+        -
+      </Button>
+      <span>{selectedProduct?.quantity}</span>
+      <Button
+        variant="primary"
+        className="h-8 flex justify-center items-center"
+        onClick={addToCartHandler}
+      >
+        +
+      </Button>
+    </div>
+  ) : (
     <Button className={className} variant="primary" onClick={addToCartHandler}>
       {loading ? <SpinnerMini /> : "افزودن به سبد خرید"}
     </Button>
